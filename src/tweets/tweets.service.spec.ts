e6fb9ddf597701c2063cfe33ea61ac96c3a822df
vipwhy12 <vipwhy12@naver.com>
2024-09-18 21:40:31 +0900

    ✅ tweets 테스트 코드 작성

--- src/tweets/tweets.service.spec.ts
+++ src/tweets/tweets.service.spec.ts
@@ -5,6 +5,8 @@ describe('TweetsService', () => {
   let service: TweetsService;
 
   beforeEach(async () => {
+    // NestJS의 내장 Test 클래스를 사용하여
+    // TweetsService만을 포함하는고립된 NestJS 런타임을 생성
     const module: TestingModule = await Test.createTestingModule({
       providers: [TweetsService],
     }).compile();
@@ -12,7 +14,37 @@ describe('TweetsService', () => {
     service = module.get<TweetsService>(TweetsService);
   });
 
+  //TwweetsService가 정의 되어있는지 확인합니다.
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('createTweet', () => {
+    it('should create tweet', () => {
+      //Arrange: 테스트 전의 설정
+      service.tweets = [];
+      const payload = 'This is my tweet';
+
+      //Act: 테스트 동작 수행
+      const tweet = service.createTweets(payload);
+
+      //Assert: 예상되는 결과 선언
+      expect(tweet).toBe(payload);
+      expect(service.tweets).toHaveLength(1);
+    });
+
+    it('should prevent tweets created which are over 100 characters', () => {
+      //Arrange: 준비단계
+      const payload =
+        '이것은 100자 이상의 문자열입니다. 이 문자열은 매우 길며, 트윗의 길이를 제한하는 조건을 테스트하기 위해 사용됩니다. 이 문자열은 매우 길며, 트윗의 길이를 제한하는 조건을 테스트하기 위해 사용됩니다. ';
+
+      //Act: 실행단계
+      const tweet = () => {
+        return service.createTweets(payload);
+      };
+
+      //Assert: 검증단계
+      expect(tweet).toThrowError();
+    });
   });
 });
--- src/tweets/tweets.service.ts
+++ src/tweets/tweets.service.ts
@@ -1,4 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { errorMonitor } from 'events';
 
 @Injectable()
-export class TweetsService {}
+export class TweetsService {
+  tweets: string[] = [];
+
+  // 1. 유효한 트윗이 생성되면, 해당 트윗을 상태에 추가해야한다.
+  // 2. 유효한 트윗이 생성되면, 메서드는 해당 트윗을 반환해야합니다.
+  // 3. 100자 이상의 트윗은 허용되지 않아야 합니다.
+  createTweets(tweet: string) {
+    if (tweet.length > 100) {
+      throw new Error(`트윗이 너무 깁니다.😓`);
+    }
+    this.tweets.push(tweet);
+    return tweet;
+  }
+
+  updateTweets(tweet: string, id: number) {
+    const tweetToUpdate = this.tweets[id];
+    if (!tweetToUpdate) {
+      throw new Error('수정하고자 하는 트윗이 존재하지 않습니다.');
+    }
+    if (tweet.length > 100) {
+      throw new Error(`트윗이 너무 깁니다.😓`);
+    }
+
+    this.tweets[id] = tweet;
+    return this.tweets[id];
+  }
+
+  getTweets() {
+    return this.tweets;
+  }
+
+  delteTweets(id: number) {
+    const tweetToDelete = this.tweets[id];
+    if (!tweetToDelete) {
+      throw new Error(`해당 트윗은 존재하지 않습니다.`);
+    }
+
+    const delteTweets = this.tweets.slice(id, 1);
+    return delteTweets;
+  }
+}
